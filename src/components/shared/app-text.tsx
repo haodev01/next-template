f66ebcd478f3eb215f2ee7c0d6a7b1d0013f6d30
haonc <haonc@vega.com.vn>
2025-02-12 09:59:33 +0700

    add code

--- src/components/shared/app-text.tsx
+++ src/components/shared/app-text.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export const AppText = (): void | React.JSX.Element => {
   const a: number = 1;
   const b: number = 2;
+  const x = 2;
   const add = (a: number, b: number) => {
     return a + b;
   };
